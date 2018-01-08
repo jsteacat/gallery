86ff0445fc8d16615ec5f43fc5396e69a4519506
Stepan Kormilin <ubscoder@gmail.com>
2018-01-08 17:53:48 +0300

    add transition effects

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
 const ESC_KEY = 27;
 
 const setDetails = (imageUrl, titleText) => {
@@ -51,7 +53,12 @@ const hideDetails = () => {
 
 const showDetails = () => {
   'use strict';
+  const frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(() => {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
 }
 
 const addKeyPressHandler = () => {
